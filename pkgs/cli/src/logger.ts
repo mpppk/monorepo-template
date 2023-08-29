6f14d2d68490a506495682597f179f465e3d868f
niboshi <niboshiporipori@gmail.com>
2023-08-29 19:51:07 +0900

    Add common (#33)
    * Add common
    
    * npm ci

--- pkgs/cli/src/logger.ts
+++ pkgs/cli/src/logger.ts
@@ -1,22 +1,4 @@
-type LoggerConfig = {
-  space: string | number | undefined;
-};
-
-export class Logger {
-  public constructor(private config: LoggerConfig) {}
-  public json(data: any) {
-    if (typeof data === "string") {
-      console.log(data);
-      return;
-    }
-    console.log(this.toLogJson(data));
-  }
-  private toLogJson(data: any) {
-    const replacer =
-      data instanceof Error ? Object.getOwnPropertyNames(data) : undefined;
-    return JSON.stringify(data, replacer, this.config.space);
-  }
-}
+import { Logger } from "common";
 
 export let logger = new Logger({ space: 2 });
 export const setLogger = (l: Logger) => {
