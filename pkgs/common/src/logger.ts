12bb6c022ebc4aee948951b80b6a96294044be42
niboshi <niboshiporipori@gmail.com>
2023-08-30 16:01:58 +0900

    Add lib (#37)
    * Add lib
    
    * Add workflow
    
    * npm install
    
    * Fix logger type
    
    * Fix

--- pkgs/common/src/logger.ts
+++ pkgs/common/src/logger.ts
@@ -4,14 +4,14 @@ export type LoggerConfig = {
 
 export class Logger {
   public constructor(private config: LoggerConfig) {}
-  public json(data: any) {
+  public json(data: unknown) {
     if (typeof data === "string") {
       console.log(data);
       return;
     }
     console.log(this.toLogJson(data));
   }
-  private toLogJson(data: any) {
+  private toLogJson(data: unknown) {
     const replacer =
       data instanceof Error ? Object.getOwnPropertyNames(data) : undefined;
     return JSON.stringify(data, replacer, this.config.space);
